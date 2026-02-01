b0fb59833fb5a043014fd349cf40b5a5483e3793
NishanthBomma <bnish28@gmail.com>
2026-02-01 18:58:03 +0530

    Add files via upload

---
+++ Operators.js
@@ -0,0 +1,35 @@
+//Airthmetic Operators: +, -, %, *, /
+//Comparision Operators - return type is boolean: <, >, <=, >=, ==, !=
+//Pre/Post - automatic increment and decrement:  a++, ++a, a--, --a
+//Logical Operators: &&, ||
+
+let a=10
+let b=20
+c=a+b
+console.log("Addition:" + c)
+d=b-a
+console.log("Subtraction:" + d)
+
+console.log(5/2)
+console.log(11%2)
+console.log(1!=0)
+
+console.log(a++)
+console.log(++a)
+
+console.log(b--)
+console.log(--b)
+
+//AND
+if(a<c && b<d)
+    console.log(a)
+else
+    console.log(b)
+
+//OR
+if(a<c || b<d)
+    console.log(a)
+else
+    console.log(b)
+
+
